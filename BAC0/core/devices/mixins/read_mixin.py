35dd7664e58f3fe0bae9403f4eecade1d58c297b
Jop <jopteungores@gmail.com>
2025-04-11 15:21:07 +0200

    Increase points per request to 25 when segmentation is not supported

--- BAC0/core/devices/mixins/read_mixin.py
+++ BAC0/core/devices/mixins/read_mixin.py
@@ -454,7 +454,7 @@ class ReadPropertyMultiple(ReadUtilsMixin, DiscoveryUtilsMixin, RPMObjectsProces
             )
         else:
             if not self.properties.segmentation_supported:
-                points_per_request = 10
+                points_per_request = 25
 
             if discover_request[0]:
                 values = []
